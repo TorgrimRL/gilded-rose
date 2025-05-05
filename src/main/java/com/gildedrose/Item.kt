6c845ea87b0f9a9f8453261b8ba5680575bfa509
Torgrim Limstrand <torgrimlimstrand@hotmail.com>
2025-05-05 15:43:07 +0200

    Introduced age and update3 methods

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -14,35 +14,26 @@ open class BaseItem(
     quality: Int
 ) : Item(name, sellIn, quality) {
     fun update() {
-        val name = name
         update1()
 
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn = sellIn - 1
-        }
+        age()
 
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality = quality - 1
-                        }
-                    }
-                } else {
-                    quality = quality - quality
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
-        }
+        update3()
     }
 
     protected open fun update1() {
         if (quality > 0) {
-            if (name != "Sulfuras, Hand of Ragnaros") {
+            quality = quality - 1
+        }
+    }
+
+    protected open fun age() {
+        sellIn = sellIn - 1
+    }
+
+    protected open fun update3() {
+        if (sellIn < 0) {
+            if (quality > 0) {
                 quality = quality - 1
             }
         }
@@ -62,6 +53,14 @@ class Brie(
         }
     }
 
+    override fun update3() {
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1
+            }
+        }
+    }
+
 }
 
 class Sulfuras(
@@ -71,6 +70,12 @@ class Sulfuras(
 ) : BaseItem(name, sellIn, quality) {
     override fun update1() {
 
+    }
+
+    override fun age() {
+    }
+
+    override fun update3() {
     }
 
 }
@@ -95,6 +100,12 @@ class Pass(
                     quality = quality + 1
                 }
             }
+        }
+    }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            quality = quality - quality
         }
     }
 
