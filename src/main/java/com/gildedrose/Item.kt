655a4205abc0db32fe2a4dfd6b775be10d3b0fa2
Torgrim Limstrand <torgrimlimstrand@hotmail.com>
2025-05-05 15:56:59 +0200

    reorder methods and merge update1 and update3

--- src/main/java/com/gildedrose/Item.kt
+++ src/main/java/com/gildedrose/Item.kt
@@ -14,16 +14,18 @@ open class BaseItem(
     quality: Int
 ) : Item(name, sellIn, quality) {
     fun update() {
-        update1()
-
         age()
-
-        update3()
+        degrade()
     }
 
-    protected open fun update1() {
+    protected open fun degrade() {
         if (quality > 0) {
             quality = quality - 1
+        }
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality = quality - 1
+            }
         }
     }
 
@@ -31,13 +33,6 @@ open class BaseItem(
         sellIn = sellIn - 1
     }
 
-    protected open fun update3() {
-        if (sellIn < 0) {
-            if (quality > 0) {
-                quality = quality - 1
-            }
-        }
-    }
 
 }
 
@@ -46,20 +41,18 @@ class Brie(
     sellIn: Int,
     quality: Int
 ) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
             quality += 1
 
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
             if (quality < 50) {
                 quality = quality + 1
             }
         }
     }
+
 
 }
 
@@ -68,15 +61,13 @@ class Sulfuras(
     sellIn: Int,
     quality: Int
 ) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
 
     }
 
     override fun age() {
     }
 
-    override fun update3() {
-    }
 
 }
 
@@ -85,28 +76,26 @@ class Pass(
     sellIn: Int,
     quality: Int
 ) : BaseItem(name, sellIn, quality) {
-    override fun update1() {
+    override fun degrade() {
         if (quality < 50) {
             quality = quality + 1
 
-            if (sellIn < 11) {
+            if (sellIn < 10) {
                 if (quality < 50) {
                     quality = quality + 1
                 }
             }
 
-            if (sellIn < 6) {
+            if (sellIn < 5) {
                 if (quality < 50) {
                     quality = quality + 1
                 }
             }
         }
-    }
-
-    override fun update3() {
         if (sellIn < 0) {
-            quality = quality - quality
+            quality = 0
         }
     }
+
 
 }
